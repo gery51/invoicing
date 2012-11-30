ad1d4459c8d938d9188c21a5b360797ac3caf8ca
Greg <greg@gregmccarvell.com>
2012-11-30 11:18:18 -0500

    login UI shows properly

---
+++ Invoicing/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,24 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		if(WAF.directory.currentUser() == null){
+			$$('page').loadComponent('components/register.waComponent');
+			$$('login').loadComponent('components/login.waComponent');
+		} else {
+			$$('page').loadComponent('components/app.waComponent');
+			$$('login').loadComponent('components/loggedIn.waComponent');
+		}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
