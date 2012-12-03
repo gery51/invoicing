694641020ab84b2658829a247e61afec49f267a5
Greg <greg@gregmccarvell.com>
2012-12-02 20:14:10 -0500

    added minimum application size until scroll bars

--- Invoicing/WebFolder/index.waPage/scripts/index.js
+++ Invoicing/WebFolder/index.waPage/scripts/index.js
@@ -9,6 +9,24 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
+		
+		//Minimum size for app
+		var vMinW = 800;
+		var vMinH = 600;
+		$(window).resize(function(){
+			if($(window).height() < vMinH){
+				$$(CF.widgets.page).addClass('minHeight');
+			} else {
+				$$(CF.widgets.page).removeClass('minHeight');
+			}
+			if($(window).width() < vMinW){
+				$$(CF.widgets.page).addClass('minWidth');
+			} else {
+				$$(CF.widgets.page).removeClass('minWidth');
+			}
+		});
+		$(window).resize();
+		
 		if(WAF.directory.currentUser() == null){
 			$$('page').loadComponent('components/register.waComponent');
 			$$('login').loadComponent('components/login.waComponent');
