e44fe4b7844edf3eaa35d38fe73740a4ba0c6d41
Greg McCarvell <greg@gregmccarvell.com>
2012-12-06 18:13:55 -0500

    beginnings of mobile view with navigation

--- Invoicing/WebFolder/index.waPage/scripts/index-smartphone.js
+++ Invoicing/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,16 +2,49 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var button2 = {};	// @button
+	var clientsButton = {};	// @button
+	var expensesButton = {};	// @button
+	var invoicesButton = {};	// @button
+	var projectsButton = {};	// @button
+	var logoutButton = {};	// @button
+	var loginButton = {};	// @button
 // @endregion// @endlock
-
+if(WAF.directory.currentUser() !== null){
+	$$('mainNavigation').goToView(2);
+}
 // eventHandlers// @lock
 
-	button2.click = function button2_click (event)// @startlock
+	clientsButton.click = function clientsButton_click (event)// @startlock
 	{// @endlock
-//		WAF.directory.login($$('usernameInput').getValue(), $$('passwordInput').getValue());
+		$$('mainNavigation').goToView(7);
+	};// @lock
 
-//login using provided credentials from input widgets
+	expensesButton.click = function expensesButton_click (event)// @startlock
+	{// @endlock
+		$$('mainNavigation').goToView(4);
+	};// @lock
+
+	invoicesButton.click = function invoicesButton_click (event)// @startlock
+	{// @endlock
+		$$('mainNavigation').goToView(5);
+	};// @lock
+
+	projectsButton.click = function projectsButton_click (event)// @startlock
+	{// @endlock
+		$$('mainNavigation').goToView(6);
+	};// @lock
+
+	logoutButton.click = function logoutButton_click (event)// @startlock
+	{// @endlock
+		WAF.directory.logout(); //Log out session
+//		$$('mainNavigation').goToView(1); //Go to login screen
+		window.location = 'index';
+	};// @lock
+
+	loginButton.click = function loginButton_click (event)// @startlock
+	{// @endlock
+
+		//login using provided credentials from input widgets
 		WAF.directory.login($$('usernameInput').getValue(), $$('passwordInput').getValue(), {
 			onSuccess: function(event){
 				$$('mainNavigation').goToView(2);
@@ -23,6 +56,11 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("clientsButton", "click", clientsButton.click, "WAF");
+	WAF.addListener("expensesButton", "click", expensesButton.click, "WAF");
+	WAF.addListener("invoicesButton", "click", invoicesButton.click, "WAF");
+	WAF.addListener("projectsButton", "click", projectsButton.click, "WAF");
+	WAF.addListener("logoutButton", "click", logoutButton.click, "WAF");
+	WAF.addListener("loginButton", "click", loginButton.click, "WAF");
 // @endregion
 };// @endlock
